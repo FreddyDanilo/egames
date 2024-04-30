411441a6422818da21bf8fefb66c2244c9a711f9
Freddy Danilo <71949651+FreddyDanilo@users.noreply.github.com>
2024-04-30 15:34:09 +0300

    feat: add nav-bar component

---
+++ src/components/nav-bar/index.tsx
@@ -0,0 +1,31 @@
+import styles from "./styles.module.css";
+
+export function NavBar() {
+  return (
+    <div className="container">
+      <div className={styles.area}>
+        <img src="/brand.png" className={styles.brand} alt="egames logotipo" />
+
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Sobre</a>
+            </li>
+
+            <li>
+              <a href="#">Funcionalidades</a>
+            </li>
+
+            <li>
+              <a href="#">Download</a>
+            </li>
+
+            <li>
+              <a href="#">Português</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
