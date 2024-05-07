e8f1025883c00ce9c682efe7ed2a458e616a0aaf
Freddy Danilo <71949651+FreddyDanilo@users.noreply.github.com>
2024-05-07 16:55:53 +0300

    feat: add footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,11 +1,35 @@
 import { ChevronDown, Languages } from "lucide-react";
 import styles from "./styles.module.css";
+import { ITEMS } from "./items-links";
 
 export function Footer() {
   return (
     <footer className={styles.area}>
       <div className="container">
-        <div className={styles.footer}>
+        <div className={styles.flex} style={{ alignItems: "center" }}>
+          <ul className={styles.links}>
+            {ITEMS.map((item) => (
+              <li key={item.id + item.title}>
+                <a href="#">{item.title}</a>
+                {item.extra && <span>{item.extra}</span>}
+              </li>
+            ))}
+
+            <li style={{ display: "flex", gap: 16, marginTop: 16 }}>
+              <img src="/medias/instagram.png" alt="instagram" />
+              <img src="/medias/twitter.png" alt="facebook" />
+              <img src="/medias/linkedin.png" alt="facebook" />
+              <img src="/medias/facebook.png" alt="facebook" />
+            </li>
+          </ul>
+
+          <p className={styles.description}>
+            Comunidade gamer feita para você ficar por dentro das novidades da
+            industria e também se conectar com outros gamers.
+          </p>
+        </div>
+
+        <div className={styles.flex}>
           <div className={styles.languages}>
             <li className={styles.language}>
               <Languages size={22} />
---
+++ src/components/footer/items-links.ts
@@ -0,0 +1,8 @@
+export const ITEMS = [
+  { id: 1, extra: "", title: "Quem somos" },
+  { id: 2, extra: "", title: "Perguntas frequentes" },
+  { id: 3, extra: "", title: "Assessoria de Imprensa" },
+  { id: 4, extra: "temos vagas", title: "Carreira" },
+  { id: 5, extra: "", title: "Blog" },
+  { id: 6, extra: "", title: "Contato" },
+];
