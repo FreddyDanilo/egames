48ee3aadb21700f09e2fa48fbbf822c7b9ae72dd
Freddy Danilo <71949651+FreddyDanilo@users.noreply.github.com>
2024-05-10 11:43:13 +0300

    feat: add animation on community

--- src/components/social-medias/community/index.tsx
+++ src/components/social-medias/community/index.tsx
@@ -1,15 +1,28 @@
 import styles from "./styles.module.css";
+import { motion } from "framer-motion";
 
 export function Community() {
+  const cardVariants = {
+    hidden: { filter: "blur(15px)", opacity: 0, transform: "translateY(25%)" },
+    reveal: { filter: "blur(0px)", opacity: 1, transform: "translateY(0)" },
+  };
+
+  const config = {
+    variants: cardVariants,
+    transition: { duration: 1 },
+    initial: "hidden",
+    whileInView: "reveal",
+  };
+
   return (
     <div className={styles.grid}>
       <div className={styles.column}>
-        <div className={styles.r1}>
+        <motion.div className={styles.r1} {...config}>
           <img src="/community/apex.png" alt="apex" width={98} height={62} />
           <span className={styles.description}>38 mil eGamers fãs</span>
-        </div>
+        </motion.div>
 
-        <div className={styles.dota}>
+        <motion.div className={styles.dota} {...config}>
           <img src="/community/dota.png" alt="dota 2" width={193} height={38} />
 
           <span className={styles.description}>28 mil eGamers fãs</span>
@@ -19,11 +32,11 @@ export function Community() {
             alt="dota 2"
             className={styles.dota_avatar}
           />
-        </div>
+        </motion.div>
       </div>
 
       <div className={styles.column}>
-        <div className={styles.r1}>
+        <motion.div className={styles.r1} {...config}>
           <img
             src="/community/overwatch.png"
             alt="overwatch"
@@ -32,23 +45,23 @@ export function Community() {
           />
 
           <span className={styles.description}>42 mil eGamers fãs</span>
-        </div>
+        </motion.div>
 
-        <div className={styles.r1}>
+        <motion.div className={styles.r1} {...config}>
           <img src="/community/pugb.png" alt="pugb" width={150} height={51} />
 
           <span className={styles.description}>180 mil eGamers fãs</span>
-        </div>
+        </motion.div>
 
-        <div className={styles.csgo}>
+        <motion.div className={styles.csgo} {...config}>
           <img src="/community/csgo.png" alt="csgo" width={132} height={55} />
 
           <span className={styles.description}>125 mil eGamers fãs</span>
-        </div>
+        </motion.div>
       </div>
 
       <div className={styles.column}>
-        <div className={styles.fifa}>
+        <motion.div className={styles.fifa} {...config}>
           <img
             src="/community/fifa.png"
             alt="fifa 22"
@@ -57,9 +70,9 @@ export function Community() {
           />
 
           <span className={styles.description}>87 mil eGamers fãs</span>
-        </div>
+        </motion.div>
 
-        <div className={styles.r2_gradient}>
+        <motion.div className={styles.r2_gradient} {...config}>
           <img
             src="/community/cta-brand.png"
             alt="egames"
@@ -76,11 +89,11 @@ export function Community() {
           <a className={styles.link} href="#">
             Instalar aplicativo
           </a>
-        </div>
+        </motion.div>
       </div>
 
       <div className={styles.column}>
-        <div className={styles.valorant}>
+        <motion.div className={styles.valorant} {...config}>
           <img
             src="/community/valorant.png"
             alt="valorant"
@@ -95,9 +108,9 @@ export function Community() {
             alt="valorant"
             className={styles.valorant_avatar}
           />
-        </div>
+        </motion.div>
 
-        <div className={styles.r1}>
+        <motion.div className={styles.r1} {...config}>
           <img
             src="/community/roblox.png"
             alt="roblox"
@@ -106,7 +119,7 @@ export function Community() {
           />
 
           <span className={styles.description}>80 mil eGamers fãs</span>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
