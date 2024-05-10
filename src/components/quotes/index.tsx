8486572519dabe1b06690c0687bf78972c46d680
Freddy Danilo <71949651+FreddyDanilo@users.noreply.github.com>
2024-05-10 11:45:35 +0300

    feat: add animation on quotes

--- src/components/quotes/index.tsx
+++ src/components/quotes/index.tsx
@@ -2,16 +2,32 @@ import { QuoteIcon } from "lucide-react";
 import styles from "./styles.module.css";
 import { QUOTES } from "./api";
 import { Quote } from "./quote";
+import { motion } from "framer-motion";
 
 export function Quotes() {
+  const itemVariants = {
+    hidden: { filter: "blur(15px)", opacity: 0 },
+    reveal: { filter: "blur(0px)", opacity: 1 },
+  };
+
+  const config = {
+    variants: itemVariants,
+    transition: { duration: 1 },
+    initial: "hidden",
+    whileInView: "reveal",
+  };
+
   return (
     <div className={styles.area}>
       <div className="container">
         <div className={styles.header}>
-          <QuoteIcon size={36} color="#fff" />
-          <h2 className={styles.title}>
+          <motion.div {...config}>
+            <QuoteIcon size={36} color="#fff" />
+          </motion.div>
+
+          <motion.h2 className={styles.title} {...config}>
             O que estão falando da comunidade por aí
-          </h2>
+          </motion.h2>
         </div>
       </div>
 
